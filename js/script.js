c110b8fca9393951632c4c2c60906c63fc7aa3ec
Niklas Ekman <niklas@niklasekman.com>
2017-01-25 13:56:38 +0200

    Meddelandet från servern hamnar i webbläsaren

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 	var jqxhr = $.ajax( "bot.php" )
-	.done(function() {
+	.done(function(meddelande) {
 		console.log( "success" );
+		console.log( meddelande );
+		$("body").text(meddelande);
 	})
 	.fail(function() {
 		console.log( "error" );
