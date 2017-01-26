dd7891675a005e911a3855eda63081d3aae621e4
Niklas Ekman <niklas@niklasekman.com>
2017-01-26 12:58:00 +0200

    Memegenerator

--- bot.php
+++ bot.php
@@ -1,3 +1,8 @@
 <?php
 header("Content-type: text/plain; charset=utf-8");
-echo "Hej!";
+if ($_GET['meme'] == NULL) {
+	echo "Hej!";
+} else {
+	$memes = ["Thanks Obama!","9+10=21","5/7 perfect score!"];
+	echo $memes[random_int(0, count($memes) - 1)];
+}
