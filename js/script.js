50074dbd677a9ae805fd8737287b69b59474d115
Niklas Ekman <niklas@niklasekman.com>
2017-01-26 11:48:20 +0200

    Lade till dokumentation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
-	var jqxhr = $.ajax( "bot.php" )
+	// jQuery.ajax() gör en GET förfrågan till bot.php
+	var jqxhr = jQuery.ajax( "bot.php" )
+	// Funktionerna done, fail och always är "länkade" (_chained_)
+	// så att nästa funktion får information från den föregående.
+	// Som argument får funktionerna en anonym funktion som kan ses
+	// som ett kodblock {} som kan ges information via ett argument
+	// -> namnet på en variabel innanför parentesen för function()
 	.done(function(meddelande) {
+		// I denna anonyma funktion kommer svaret från sidan bot.php
+		// in som ett argument. Koden i funktionen done() kallar på denna
+		// anonyma funktion med svaret från bot.php som argument.
+		// Innehållet i svaret kan man komma åt genom variabeln med
+		// namnet meddelande
 		console.log( "success" );
 		console.log( meddelande );
-		$("body").text(meddelande);
+		$("body").text( meddelande );
 	})
 	.fail(function() {
 		console.log( "error" );
